5ef3a0e3b3e0b6f1ba5faf93b621e59473004e93
James Lave <30740613+jlave-dev@users.noreply.github.com>
2018-08-06 17:57:03 -0500

    Overhaul UI

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 
-// create CSV file if it doesn't exist
-const csvExists = fs.existsSync('./humidity_test.csv');
-
-if (!csvExists) {
-  fs.writeFileSync('./humidity_test.csv', 'timestamp,sensor,humidity,temperature\n');
+// Create CSV files if they don't exist
+if (!fs.existsSync('./inlet.csv')) {
+  fs.writeFileSync('./inlet.csv', 'timestamp,humidity,temperature');
+}
+if (!fs.existsSync('./outlet.csv')) {
+  fs.writeFileSync('./outlet.csv', 'timestamp,humidity,temperature');
 }
 
 app.get('/', (req, res) => {
@@ -24,23 +25,57 @@ app.get('/download', (req, res) => {
   res.send(csvData);
 });
 
-// handle requests
+app.get('/most-recent', async (req, res) => {
+  try {
+    const inletData = await fs.readFile('./inlet.csv', 'utf-8');
+    const outletData = await fs.readFile('./outlet.csv', 'utf-8');
+
+    const splitInletData = inletData.split('\n');
+    const recentInletArray = splitInletData.length > 1 ?
+      splitInletData[splitInletData.length - 1].split(',') :
+      null;
+    const recentInlet = recentInletArray ? {
+      timestamp: new Date(recentInletArray[0]),
+      humidity: parseFloat(recentInletArray[1]),
+      temperature: parseFloat(recentInletArray[2]),
+    } : null;
+
+    const splitOutletData = outletData.split('\n');
+    const recentOutletArray = splitOutletData.length > 1 ?
+      splitOutletData[splitOutletData.length - 1].split(',') :
+      null;
+    const recentOutlet = recentOutletArray ? {
+      timestamp: new Date(recentOutletArray[0]),
+      humidity: parseFloat(recentOutletArray[1]),
+      temperature: parseFloat(recentOutletArray[2]),
+    } : null;
+
+    const reading = {
+      ...({ inlet: recentInlet } || {}),
+      ...({ outlet: recentOutlet } || {}),
+    };
+
+    res.send(reading);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+// Handle GET request containing a reading
 app.get('/sensor', (req, res) => {
-  console.log(req.query);
-
-  // get values from query
   const { sensor, humidity, temperature } = req.query;
+
+  // Send relevant object to frontend
   const timestamp = new Date();
+  const reading = { [sensor]: { timestamp, humidity, temperature } };
+  io.sockets.emit('reading', reading);
 
-  const reading = {timestamp, sensor, humidity, temperature};
+  // Build CSV string for backend
+  const csvData = `\n${timestamp},${humidity},${temperature}`;
 
-  io.sockets.emit('reading:received', reading);
-
-  // build CSV string
-  const csvData = `${timestamp},${sensor},${humidity},${temperature}\n`;
-
-  // append to end of humidity_test.csv
-  fs.appendFile('./humidity_test.csv', csvData, (err) => {
+  // Append string to end of appropriate CSV file
+  fs.appendFile(`./${sensor}.csv`, csvData, (err) => {
     if (err) return res.sendStatus(500);
     return res.sendStatus(200);
   });

bde1a0281eacb275d9401fdbdc7eac91496ff772
James Lave <30740613+jlave-dev@users.noreply.github.com>
2020-01-14 02:19:37 -0600

    Change ssid and password via serial commands

--- hih-sensor.ino
+++ hih-sensor.ino
@@ -1,13 +1,16 @@
+#include <string.h>
 #include <Wire.h>
 #include <ESP8266WiFi.h>
 #include "HIH91XX.h"
 
 HIH91XX hih(0x27);
 
-const char* ssid     = "Mandana's iPhone";
-const char* password = "h3b0ic17va6a6";
+const String sensor     = "inlet";
+const char* host        = "us-central1-hih-sensor.cloudfunctions.net";
+const int loopDelayInMs = 60000;
 
-const char* host = "172.20.10.5";
+char ssid[50]     = "SweetHome-2.4";
+char password[50] = "bestpersiancucumber1989";
 
 void setup()
 {
@@ -17,6 +20,24 @@ void setup()
 
 void loop()
 {
+  if(Serial.available()){
+    String command = Serial.readStringUntil(' ');
+    String value = Serial.readStringUntil('\n');
+
+    char valueCharArray[50];
+    value.toCharArray(valueCharArray, 50);
+
+    if(command.equals("set_ssid")){
+        strcpy(ssid, valueCharArray);
+    }
+    else if(command.equals("set_pass")){
+        strcpy(password, valueCharArray);
+    }
+    else{
+        Serial.println("Invalid command");
+    }
+  }
+
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
@@ -27,25 +48,27 @@ void loop()
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(1000);
     Serial.print(".");
   }
 
   Serial.println("");
-  Serial.println("WiFi connected");  
+  Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
+  const int httpPort = 80;
   Serial.print("connecting to ");
-  Serial.println(host);
+  Serial.print(host);
+  Serial.print(":" + String(httpPort) + "\n");
 
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
-  const int httpPort = 8080;
   if (!client.connect(host, httpPort)) {
     Serial.println("connection failed");
     return;
   }
+
   // sensor code
   hih.start();
   hih.update();
@@ -54,25 +77,24 @@ void loop()
   float temp = hih.temperature();
   String humidityStr = String(humidity);
   String tempStr = String(temp);
-  Serial.println(humidity);
-  Serial.println(humidityStr);
 
   //Serial.println(100 * hih.humidity());
   //Serial.print(hih.temperature(), 5);
-  
+
   // We now create a URI for the request
-  String url = "/test?";
+  String url = "/api/sensor?";
   url += "humidity=" + humidityStr;
   url += "&temperature=" + tempStr;
-  
+  url += "&sensor=" + sensor;
+
   Serial.print("Requesting URL: ");
   Serial.println(url);
-  
+
   // This will send the request to the server
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n");
-               
+               "Host: " + host + "\r\n" +
+               "Connection: keep-alive\r\n\r\n");
+
   unsigned long timeout = millis();
   while (client.available() == 0) {
     if (millis() - timeout > 5000) {
@@ -81,7 +103,7 @@ void loop()
       return;
     }
   }
-  
+
   // Read all the lines of the reply from server and print them to Serial
   while(client.available()){
     String line = client.readStringUntil('\r');
@@ -89,5 +111,5 @@ void loop()
   }
 
   hih.stop();
-  delay(1000);
+  delay(loopDelayInMs);
 }

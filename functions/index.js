bc5fd90a5874e98017a12b75000178a3f3e60eb6
James Lave <30740613+jlave-dev@users.noreply.github.com>
2020-01-14 02:19:21 -0600

    Hook up to Firebase

---
+++ functions/index.js
@@ -0,0 +1,92 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const xlsx = require('xlsx');
+const serviceAccount = require('./hih-sensor-firebase-adminsdk-kmoz1-558a68e0dc.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://hih-sensor.firebaseio.com'
+});
+
+const express = require('express');
+const app = express();
+
+app.get('/download/:sensor', async (req, res) => {
+  try {
+    const { sensor } = req.params;
+
+    const sensorSnapshot = await admin.firestore()
+      .collection(`sensors/${sensor}/readings`)
+      .orderBy('time', 'desc')
+      .get();
+
+    const json = sensorSnapshot.docs.map(doc => {
+      const data = doc.data();
+      data.time = data.time.toDate();
+      return data;
+    })
+
+    const wb = xlsx.utils.book_new();
+    const ws = xlsx.utils.json_to_sheet(json);
+    xlsx.utils.book_append_sheet(wb, ws);
+    const out = xlsx.write(wb, { bookType: 'xlsx', type: 'buffer' });
+    res.set({
+      'Content-Disposition': `attachment; filename=${sensor}.xlsx`,
+    });
+    res.type('xlsx').send(new Buffer(out));
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
+
+app.get('/clear/:sensor', async (req, res) => {
+  try {
+    const { sensor } = req.params;
+    await admin.firestore()
+      .collection('sensors')
+      .doc(sensor)
+      .set({
+        earliestReading: admin.firestore.FieldValue.serverTimestamp(),
+      });
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
+
+// Handle GET request containing a reading
+app.get('/sensor', async (req, res) => {
+  const { sensor, humidity, temperature } = req.query;
+
+  try {
+    // Save to Firestore
+    const sensorSnapshot = await admin.firestore()
+      .collection('sensors')
+      .doc(sensor)
+      .get();
+
+    if (!sensorSnapshot.exists) {
+      await sensorSnapshot.ref.set({
+        earliestReading: admin.firestore.FieldValue.serverTimestamp(),
+      });
+    }
+
+    await sensorSnapshot.ref.collection('readings')
+      .doc()
+      .create({
+        time: admin.firestore.FieldValue.serverTimestamp(),
+        humidity: parseFloat(humidity),
+        temperature: parseFloat(temperature),
+      });
+
+    return res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(error);
+  }
+});
+
+exports.api = functions.https.onRequest(app);

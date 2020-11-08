35a83282a816bce20558d37f0f829830b8cef6f1
James Lave <30740613+jlave-dev@users.noreply.github.com>
2020-11-07 22:49:31 -0600

    More sensibly format downloaded data

--- functions/index.js
+++ functions/index.js
@@ -1,64 +1,66 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const xlsx = require('xlsx');
-const serviceAccount = require('./hih-sensor-firebase-adminsdk-kmoz1-558a68e0dc.json');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const xlsx = require("xlsx");
+const serviceAccount = require("./hih-sensor-firebase-adminsdk-kmoz1-558a68e0dc.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://hih-sensor.firebaseio.com'
+  databaseURL: "https://hih-sensor.firebaseio.com",
 });
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/download/:sensor', async (req, res) => {
+app.get("/download/:sensor", async (req, res) => {
   try {
     const { sensor } = req.params;
 
-    const sensorSnapshot = await admin.firestore()
-      .collection('sensors')
+    const sensorSnapshot = await admin
+      .firestore()
+      .collection("sensors")
       .doc(sensor)
       .get();
 
     const earliestReading = sensorSnapshot.exists
-      ? sensorSnapshot.get('earliestReading') || new Date()
+      ? sensorSnapshot.get("earliestReading") || new Date()
       : new Date();
 
     const readings = await sensorSnapshot.ref
-      .collection('readings')
-      .where('time', '>', earliestReading)
-      .orderBy('time', 'desc')
+      .collection("readings")
+      .where("time", ">", earliestReading)
+      .orderBy("time", "asc")
       .get();
 
-    const json = readings.docs.map(doc => {
+    const json = readings.docs.map((doc) => {
       const data = doc.data();
-      data.time = data.time.toDate();
+      data.time = data.time
+        .toDate()
+        .toLocaleString("en-US", { timeZone: "America/Chicago" });
       return data;
-    })
+    });
+
+    console.log(json.slice(10));
 
     const wb = xlsx.utils.book_new();
     const ws = xlsx.utils.json_to_sheet(json);
     xlsx.utils.book_append_sheet(wb, ws);
-    const out = xlsx.write(wb, { bookType: 'xlsx', type: 'buffer' });
+    const out = xlsx.write(wb, { bookType: "xlsx", type: "buffer" });
     res.set({
-      'Content-Disposition': `attachment; filename=${sensor}.xlsx`,
+      "Content-Disposition": `attachment; filename=${sensor}.xlsx`,
     });
-    res.type('xlsx').send(new Buffer(out));
+    res.type("xlsx").send(new Buffer(out));
   } catch (error) {
     console.error(error);
     res.status(500).send(error);
   }
 });
 
-app.get('/clear/:sensor', async (req, res) => {
+app.get("/clear/:sensor", async (req, res) => {
   try {
     const { sensor } = req.params;
-    await admin.firestore()
-      .collection('sensors')
-      .doc(sensor)
-      .set({
-        earliestReading: admin.firestore.FieldValue.serverTimestamp(),
-      });
+    await admin.firestore().collection("sensors").doc(sensor).set({
+      earliestReading: admin.firestore.FieldValue.serverTimestamp(),
+    });
 
     res.sendStatus(200);
   } catch (error) {
@@ -68,13 +70,14 @@ app.get('/clear/:sensor', async (req, res) => {
 });
 
 // Handle GET request containing a reading
-app.get('/sensor', async (req, res) => {
+app.get("/sensor", async (req, res) => {
   const { sensor, humidity, temperature } = req.query;
 
   try {
     // Save to Firestore
-    const sensorSnapshot = await admin.firestore()
-      .collection('sensors')
+    const sensorSnapshot = await admin
+      .firestore()
+      .collection("sensors")
       .doc(sensor)
       .get();
 
@@ -84,7 +87,8 @@ app.get('/sensor', async (req, res) => {
       });
     }
 
-    await sensorSnapshot.ref.collection('readings')
+    await sensorSnapshot.ref
+      .collection("readings")
       .doc()
       .create({
         time: admin.firestore.FieldValue.serverTimestamp(),
